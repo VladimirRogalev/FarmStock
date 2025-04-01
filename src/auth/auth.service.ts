c69ef657642681ebc263193cea9b026f19606c83
Vladimir Rogalev <vladimirrogalev1@gmail.com>
2025-04-01 23:21:32 +0300

    created addRefreshTokenToResponse and removeRefreshTokenFromResponse

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,20 @@
-import {BadRequestException, Injectable, NotFoundException} from '@nestjs/common';
+import {BadRequestException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
 import {JwtService} from '@nestjs/jwt';
 import {CustomerService} from '../customer/customer.service';
 import {PrismaService} from '../prisma.service';
 import {AuthDto} from './dto/auth.dto';
+import {Response} from 'express';
+import {ConfigService} from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    constructor(private jwt: JwtService, private customerService: CustomerService, private prisma: PrismaService) {
+    EXPIRE_DAY_REFRESH_TOKEN = 1;
+    REFRESH_TOKEN_NAME = 'refreshToken';
+
+    constructor(private jwt: JwtService,
+                private customerService: CustomerService,
+                private prisma: PrismaService,
+                private configService: ConfigService) {
     }
 
     async login(dto: AuthDto) {
@@ -26,6 +34,20 @@ export class AuthService {
         return {customer, ...tokens};
     }
 
+    async getNewTokens(refreshToken: string) {
+        const result = await this.jwt.verifyAsync(refreshToken);
+        if (!result) {
+            throw new UnauthorizedException('Refresh token in not a valid');
+        }
+        const customer = await this.customerService.getById(result.id);
+        if (!customer) {
+            throw new UnauthorizedException('Customer not found');
+        }
+        const tokens = this.issueTokens(customer.id);
+        return {customer, ...tokens};
+
+    }
+
     issueTokens(customerId: string) {
         const data = {id: customerId};
         const accessToken = this.jwt.sign(data, {
@@ -43,5 +65,43 @@ export class AuthService {
             throw new NotFoundException('Customer not found');
         }
         return customer;
+    }
+
+    async validateOAuthLogin(req: any) {
+        let customer = await this.customerService.getByEmail(req.customer.email);
+
+        if (!customer) {
+            customer = await this.prisma.customer.create({
+                data: {
+                    email: req.customer.email,
+                    firstName: req.customer.givenName,
+                    lastName: req.customer.familyName
+                }
+            });
+        }
+        const tokens = this.issueTokens(customer.id);
+        return {customer, ...tokens};
+    }
+
+    addRefreshTokenToResponse(res: Response, refreshToken: string) {
+        const expiresIn = new Date();
+        expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
+        res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+            httpOnly: true,
+            domain: this.configService.get('SERVER_DOMAIN'),
+            expires: expiresIn,
+            secure: true,
+            sameSite: 'none'
+        });
+    }
+
+    removeRefreshTokenFromResponse(res: Response) {
+        res.cookie(this.REFRESH_TOKEN_NAME, '', {
+            httpOnly: true,
+            domain: this.configService.get('SERVER_DOMAIN'),
+            expires: new Date(0),
+            secure: true,
+            sameSite: 'none'
+        });
     }
 }
