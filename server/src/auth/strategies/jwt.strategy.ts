46d785b103bcd63c53f6746139c716d4adc0c62e
Vladimir Rogalev <vladimirrogalev1@gmail.com>
2025-05-21 21:10:41 +0300

    fixed jwt.strategy

--- server/src/auth/strategies/jwt.strategy.ts
+++ server/src/auth/strategies/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: true,
-            secretOrKey: configService.get('JWT_SECRET')!
+            secretOrKey: configService.get(`${process.env.JWT_SECRET}`)!
         });
     }
 
